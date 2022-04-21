d663c6e5bcc8f8e72a0e35e088b2e3408b86e51c
Dennis Brown <dennis@Denniss-MacBook-Pro.local>
2022-04-21 14:10:59 -0500

    latest data

--- analysis/surveyanalyze.py
+++ analysis/surveyanalyze.py
@@ -1,7 +1,7 @@
 import sys 
 import pandas as pd
 import numpy as np
-
+from scipy.stats import mannwhitneyu
 
 # Relative paths = run this module from where it lives
 datafilename = 'Automatically Generated Code Summary Survey (Responses) - Form Responses 1.csv'
@@ -49,8 +49,19 @@ def main():
         print('Rel avg exp{}: {}'.format(exp, relAvgs[exp].mean()))
         print('Sim avg exp{}: {}'.format(exp, simAvgs[exp].mean()))
         print('Nat avg exp{}: {}'.format(exp, natAvgs[exp].mean()))
-        print()
 
+        if (exp > 0):
+            maxU = len(relAvgs[exp]) * len(relAvgs[exp])
+            U1, p = mannwhitneyu(relAvgs[exp], relAvgs[0], method="exact")
+            U = min(U1, maxU - U1)
+            print('Rel vs baseline U = {}, p = {}'.format(U, p))
+            U1, p = mannwhitneyu(simAvgs[exp], simAvgs[0], method="exact")
+            U = min(U1, maxU - U1)
+            print('Sim vs baseline U = {}, p = {}'.format(U, p))
+            U1, p = mannwhitneyu(natAvgs[exp], natAvgs[0], method="exact")
+            U = min(U1, maxU - U1)
+            print('Nat vs baseline U = {}, p = {}'.format(U, p))
+        print()
 
 
 if __name__ == '__main__':
