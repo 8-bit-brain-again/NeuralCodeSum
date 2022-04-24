abbf4cdc152f19bda365e4865c975863784fe312
dennisgbrown <dennis.g.brown@gmail.com>
2022-04-23 20:50:22 -0500

    more data and more stat analysis

--- analysis/surveyanalyze.py
+++ analysis/surveyanalyze.py
@@ -1,7 +1,9 @@
 import sys 
 import pandas as pd
 import numpy as np
+import scipy.stats
 from scipy.stats import mannwhitneyu
+from scipy.stats import ttest_ind
 
 # Relative paths = run this module from where it lives
 datafilename = 'Automatically Generated Code Summary Survey (Responses) - Form Responses 1.csv'
@@ -21,6 +23,16 @@ numExperiments = 3
 #             codingExps.append(float(values[1]))
 #             javaExps.append(float(values[2]))
 #             comments.append(values[-1])
+
+# Sourced from https://www.statology.org/f-test-python/
+def f_test(x, y):
+    x = np.array(x)
+    y = np.array(y)
+    f = np.var(x, ddof=1)/np.var(y, ddof=1) #calculate F test statistic 
+    dfn = x.size-1 #define degrees of freedom numerator 
+    dfd = y.size-1 #define degrees of freedom denominator 
+    p = 1-scipy.stats.f.cdf(f, dfn, dfd) #find p-value of F test statistic 
+    return f, p
 
 
 def main():
@@ -63,6 +75,33 @@ def main():
             U1, p = mannwhitneyu(natAvgs[exp], natAvgs[0], method="exact")
             U = min(U1, maxU - U1)
             print('Nat vs baseline U = {}, p = {}'.format(U, p))
+
+            f, f_p = f_test(relAvgs[exp], relAvgs[0])
+            if (p > 0.05):
+                s, p = ttest_ind(relAvgs[exp], relAvgs[0], equal_var=True)
+                print('Rel vs baseline s = {}, p = {}, F-test p = {}'.format(s, p, f_p))
+            else:
+                s, p = ttest_ind(relAvgs[exp], relAvgs[0], equal_var=False)
+                print('Rel vs baseline s = {}, p = {}, F-test p = {}'.format(s, p, f_p))
+
+            f, f_p = f_test(simAvgs[exp], simAvgs[0])
+            if (p > 0.05):
+                s, p = ttest_ind(simAvgs[exp], simAvgs[0], equal_var=True)
+                print('Rel vs baseline s = {}, p = {}, F-test p = {}'.format(s, p, f_p))
+            else:
+                s, p = ttest_ind(simAvgs[exp], simAvgs[0], equal_var=False)
+                print('Rel vs baseline s = {}, p = {}, F-test p = {}'.format(s, p, f_p))
+
+            f, f_p = f_test(natAvgs[exp], natAvgs[0])
+            if (p > 0.05):
+                s, p = ttest_ind(natAvgs[exp], natAvgs[0], equal_var=True)
+                print('Rel vs baseline s = {}, p = {}, F-test p = {}'.format(s, p, f_p))
+            else:
+                s, p = ttest_ind(natAvgs[exp], natAvgs[0], equal_var=False)
+                print('Rel vs baseline s = {}, p = {}, F-test p = {}'.format(s, p, f_p))
+
+
+
         print()
 
 
